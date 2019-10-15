f54e1aa7ebe59eaa013e60be27b42b7429489e50
elvirasun28 <elvirasun28@outlook.com>
2019-10-15 04:23:09 -0400

    updated by SX

--- DCN/main.py
+++ DCN/main.py
@@ -25,14 +25,65 @@ def load_data():
     X_train = dfTrain[cols].values
     y_train = dfTrain['target'].values
     X_test = dfTest[cols].values
-    y_test = dfTest['target'].values
+    ids_test = dfTest['id'].values
 
-    return dfTrain,dfTest,X_train,y_train,X_test,y_test
+    return dfTrain,dfTest,X_train,y_train,X_test,ids_test
 
 
 def run_base_model_dcn(dfTrain,dfTest,folds,dcn_params):
+    fd = FeatureDictionary(dfTrain,dfTest,numeric_cols=config.NUMERIC_COLS,
+                           ignore_cols=config.IGNORE_COLS,cate_cols=config.CATEGORICAL_COLS)
+    print(fd.feat_dim)
+    print(fd.feat_dict)
+
+    data_parser = DataParser(feat_dict=fd)
+    cate_Xi_train, cate_Xv_train,numeric_Xv_train,y_train = data_parser.parse(df=dfTrain,has_label=True)
+    cate_Xi_test,cate_Xv_test,numeric_Xv_test,ids_test = data_parser.parse(df=dfTest,has_label=False)
+
+    dcn_params['cate_feature_size'] = fd.feat_dim
+    dcn_params['field_size'] = len(cate_Xi_train[0])
+    dcn_params['numeric_feature_size'] = len(config.NUMERIC_COLS)
+
+    _get = lambda x, l:[x[i] for i in l]
+
+    for i, (train_idx, valid_idx) in enumerate(folds):
+        cate_Xi_train_,cate_Xv_train_, numeric_Xv_train_,y_train_ = _get(cate_Xi_train,train_idx),_get(cate_Xv_train, train_idx),_get(numeric_Xv_train, train_idx), _get(y_train, train_idx)
+        cate_Xi_valid_, cate_Xv_valid_, numeric_Xv_valid_, y_valid_ = _get(cate_Xi_train, valid_idx), _get(cate_Xv_train, valid_idx), _get(numeric_Xv_train, valid_idx), _get(y_train, valid_idx)
+
+        dcn = DCN(**dcn_params)
+        dcn.fit(cate_Xi_train_, cate_Xv_train_, numeric_Xv_train_, y_train_, cate_Xi_valid_, cate_Xv_valid_,
+                numeric_Xv_valid_, y_valid_)
 
 
 
+
+## start lines....
+dfTrain, dfTest, X_train, y_train, X_test, ids_test = load_data()
+print('load data over...')
+folds = list(StratifiedKFold(n_splits=config.NUM_SPLITS,
+                             shuffle=True,
+                             random_state=config.RANDOM_SEED).split(X_train,y_train)
+             )
+print('process data over ...')
+
+dcn_params = {
+"embedding_size": 8,
+    "deep_layers": [32, 32],
+    "dropout_deep": [0.5, 0.5, 0.5],
+    "deep_layers_activation": tf.nn.relu,
+    "epoch": 30,
+    "batch_size": 1024,
+    "learning_rate": 0.001,
+    "optimizer_type": "adam",
+    "batch_norm": 1,
+    "batch_norm_decay": 0.995,
+    "l2_reg": 0.01,
+    "verbose": True,
+    "random_seed": config.RANDOM_SEED,
+    "cross_layer_num":3
+}
+
+print('start train ....')
+run_base_model_dcn(dfTrain,dfTest,folds,dcn_params)
 
 
